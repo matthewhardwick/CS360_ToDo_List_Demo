8d9bda0115c6d99602d7e83bef5544a170497d8d
Matthew Hardwick <mhardwick@vertigo.com>
2013-11-12 01:43:30 -0800

    Added Click Listeners to the listFragment

--- TodoList/src/main/java/io/hardwick/todolist/TodoListFragment.java
+++ TodoList/src/main/java/io/hardwick/todolist/TodoListFragment.java
@@ -2,16 +2,20 @@ package io.hardwick.todolist;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class TodoListFragment extends Fragment {
+public class TodoListFragment extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener{
     Context context;
     ArrayAdapter<String> adapter;
     ListView listView;
@@ -29,14 +33,37 @@ public class TodoListFragment extends Fragment {
         view = inflater.inflate(R.layout.todo_list_view, container, false);
 
         listView = (ListView) view.findViewById(R.id.todo_listview);
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_multiple_choice, items);
+        adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_multiple_choice, items);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
 
         return view;
     }
 
     public void addItem(String item) {
         adapter.add(item);
-        adapter.notifyDataSetInvalidated();
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        CheckedTextView checkedTextView = (CheckedTextView) view;
+        if (checkedTextView.isChecked()) {
+            Toast.makeText(getActivity(),adapter.getItem(i).toString() + " completed",Toast.LENGTH_SHORT).show();
+            checkedTextView.setPaintFlags(checkedTextView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            checkedTextView.setChecked(false);
+        } else {
+            Toast.makeText(getActivity(),adapter.getItem(i).toString() + " removed from completion",Toast.LENGTH_SHORT).show();
+            checkedTextView.setPaintFlags(checkedTextView.getPaintFlags() & ( ~ Paint.STRIKE_THRU_TEXT_FLAG));
+            checkedTextView.setChecked(true);
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        adapter.remove(adapter.getItem(i));
+        adapter.notifyDataSetChanged();
+        return true;
     }
 }
